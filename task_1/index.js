c6b8d9777a55d169517c36805c9d8f38b2ecc57d
Your Name <you@example.com>
2024-05-16 12:00:13 +0200

    done

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,28 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decodeFields(encoded, translations) {
+  const decodedList = [];
+  const uniqueIds = new Set()
+  for (const item of encoded) {
+    const decodedItem = {};
+    for (const key in item) {
+      if (key === "groupId" || key === "service" || key === "formatSize" || key === "ca") {
+        decodedItem[key] = item[key];
+        if (key === "groupId")
+          uniqueIds.add(item.groupId)
+      } else if (key.endsWith("Id")) {
+        uniqueIds.add(item[key]===null?item[key]:parseInt(item[key]))
+        const translationKey = parseInt(item[key]);
+        decodedItem[key] = translations[translationKey] || item[key];
+      }
+    }
+    decodedList.push(decodedItem);
+  }
 
+  return { decodedList, uniqueIds };
+}
 
-
-// console.log(decoded)
+// Пример использования
+const { decodedList, uniqueIds } = decodeFields(encoded, translations);
+console.log("Расшифрованные поля:", decodedList);
+console.log("Уникальные идентификаторы:", uniqueIds);
--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,43 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  import { onMount } from 'svelte';
+  let fromCurrency = 'USD';
+  let toCurrency = 'EUR';
+  let amountToConvert = '1';
+  let convertedAmount = '0';
+  let rates = {};
+
+  const fetchRates = async (base = 'USD') => {
+    const res = await fetch(`https://api.exchangerate-api.com/v4/latest/${base}`);
+    const data = await res.json();
+    rates = data.rates;
+  };
+
+  const convertCurrency = (direction = 'forward') => {
+    if (direction === 'forward') {
+      convertedAmount = (parseFloat(amountToConvert) * rates[toCurrency]).toFixed(2);
+    } else {
+      amountToConvert = (parseFloat(convertedAmount) / rates[toCurrency]).toFixed(2);
+    }
+  };
+
+  onMount(async () => {
+    await fetchRates(fromCurrency);
+    convertCurrency();
+  });
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div>
+  <input type="number" bind:value={amountToConvert} on:input="{() => convertCurrency('forward')}" min="0">
+  <select bind:value={fromCurrency} on:change="{async () => { await fetchRates(fromCurrency); convertCurrency(); }}">
+    {#each Object.keys(rates) as currency}
+      <option>{currency}</option>
+    {/each}
+  </select>
+  <span>=</span>
+  <input type="number" bind:value={convertedAmount} on:input="{() => convertCurrency('backward')}" min="0">
+  <select bind:value={toCurrency} on:change="{()=>convertCurrency()}">
+    {#each Object.keys(rates) as currency}
+      <option>{currency}</option>
+    {/each}
+  </select>
+</div>
